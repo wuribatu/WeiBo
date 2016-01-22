b339894002a7bd8367493dab7ba7e256b27c19ae
乌日巴图 <wuribatu_iphone@yeah.net>
2016-01-22 22:20:54 +0800

    动态创建类

--- WeiBo/WeiBo/Classes/Main/MainViewController.swift
+++ WeiBo/WeiBo/Classes/Main/MainViewController.swift
@@ -15,10 +15,27 @@ class MainViewController: UITabBarController {
         
         tabBar.tintColor = UIColor.orangeColor()
         
-        addChildViewController("HomeTableViewController",     title: "首页", imageName: "tabbar_home")
-        addChildViewController("MessageTableViewController",  title: "消息", imageName: "tabbar_message_center")
-        addChildViewController("DiscoverTableViewController", title: "广场", imageName: "tabbar_discover")
-        addChildViewController("ProfileTableViewController",  title: "我",   imageName: "tabbar_profile")
+        let path = NSBundle.mainBundle().pathForResource("MainVCSettings.json", ofType: nil)
+        
+        if let jsonPath = path {
+            let jsonData = NSData(contentsOfFile: jsonPath)
+            
+            do {//有可能发生异常的代码放到这里
+                //try  发生异常会调到catch中继续执行
+                //try! 发生异常直接崩溃
+                let dictArr = try NSJSONSerialization.JSONObjectWithData(jsonData!, options: NSJSONReadingOptions.MutableContainers)
+                for dict in dictArr as! [[String: String]]{
+                    addChildViewController(dict["vcName"]!, title: dict["title"]!, imageName: dict["imageName"]!)
+                }
+                
+            } catch {
+                print(error)
+                addChildViewController("HomeTableViewController",     title: "首页", imageName: "tabbar_home")
+                addChildViewController("MessageTableViewController",  title: "消息", imageName: "tabbar_message_center")
+                addChildViewController("DiscoverTableViewController", title: "广场", imageName: "tabbar_discover")
+                addChildViewController("ProfileTableViewController",  title: "我",   imageName: "tabbar_profile")
+            }
+        }
     }
     
     private func addChildViewController(childControllerName: String, title: String, imageName: String) {
